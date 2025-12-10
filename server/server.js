b32b7a1c963e620dbc7046b3e22936200f5525b5
DinhGH <huynhdinh2k52707@gmail.com>
2025-12-10 13:53:16 +0700

    cors

--- server/server.js
+++ server/server.js
@@ -8,7 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const client = new Groq({
